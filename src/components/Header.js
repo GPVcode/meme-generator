f57160a9656ab2d34bd0f1b2be422af61c49f63a
GPV <gvill005@gmail.com>
2023-01-26 13:34:55 -0800

    updated logo and centered meme image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
 import React from "react";
 import trollFace from "../images/troll-face.png"
-
+import logo from "../images/2.png"
 
 const Header = () => {
 
     return(
         <header className="header">
-            <img src={trollFace} className="header--image" alt=""/>
+            <img src={trollFace} className="header--image" alt="trollface"/>
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <img src={logo} className="header--logo" alt="logo"/>
+            {/* <h4 className="header--project">GPVdev</h4> */}
         </header>
     )
 
